cdaa64c21dff0a39e2d74d5ec068b88c2e0b9a06
funny-walnut <valeriya.zinoveva@optimax.dev>
2023-09-23 23:15:40 +0400

    add Header, Navbar and Profile components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg"
+             alt='logo'/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return  <div className='content'>
+        <div>
+            <img src='https://wallpapers.com/images/featured/nature-2ygv7ssy2k0lxlzu.jpg' alt='nature'/>
+        </div>
+        <div>
+            <img
+                src='https://static.vecteezy.com/system/resources/previews/025/284/015/non_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg'
+                alt='ava'/>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>New post</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+        main content
+
+    </div>
+}
+
+export default Profile;
